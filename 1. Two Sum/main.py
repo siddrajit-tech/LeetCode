d62a025dca6a21d747a5916ba5b86183741ad0a3
Siddharth Rajit <siddrajit@gmail.com>
2025-11-22 18:00:15 -0400

    Completed 1. Two Sum

--- 1. Two Sum/main.py
+++ 1. Two Sum/main.py
@@ -1,14 +1,5 @@
 def twoSum(nums: list[int], target: int) -> list[int]:
-
-  num = 0
-
   for n in nums:
-    if n < target:
-      num += target - n
-    
-  if num in nums:
-    return [nums[num], nums[n]]
-    
-print(twoSum([15, 7, 2, 11], 9))
+    return n
     
   
